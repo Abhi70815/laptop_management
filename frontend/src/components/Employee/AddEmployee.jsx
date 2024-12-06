17fead93c9885c8b8eb9529c162bb068b3882001
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-12-06 21:45:15 +0530

    save

--- frontend/src/components/Employee/AddEmployee.jsx
+++ frontend/src/components/Employee/AddEmployee.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import './AddEmployee.css'; // Import the CSS file
+import './addEmployee.css'; // Import the CSS file
 
 const AddEmployee = () => {
   const navigate = useNavigate();
